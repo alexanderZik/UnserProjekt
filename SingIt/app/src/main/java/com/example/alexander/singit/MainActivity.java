ca7927586750022d2d31c42b5639d425d8d6c1ef
azike <alexander.zikulnig@gmx.at>
2018-08-02 00:43:48 +0200

    update Test 1

--- SingIt/app/src/main/java/com/example/alexander/singit/MainActivity.java
+++ SingIt/app/src/main/java/com/example/alexander/singit/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.alexander.singit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+
+    public void onClick(View v){
+        TextView txt = (TextView)findViewById(R.id.txtV);
+        txt.append("Hello");
     }
 }
